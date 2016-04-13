c4070cfc3fe13e9fc6a9e7fe913900b8f09f25f4
Lincoln Mullen <lincoln@lincolnmullen.com>
2016-04-13 18:39:40 -0400

    Turn list column into character vector

--- data-raw/data-prep.R
+++ data-raw/data-prep.R
@@ -7,7 +7,7 @@ library(tokenizers)
 paths <- list.files("data-raw/tracts", full.names = TRUE)
 files <- basename(paths)
 id <- str_replace_all(files, "\\.txt", "")
-texts <- lapply(paths, read_file)
+texts <- lapply(paths, read_file) %>% unlist()
 
 tracts_for_the_times <- data_frame(id = id,
                                    number = as.integer(str_extract(id, "\\d+")),
